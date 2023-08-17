818b5d19ccc3f5fe216b895a7a84f566bfac9028
Mor Shaar <morsrs1@gmail.com>
2023-08-17 12:42:16 +0300

    general-ui: add vertical scroll to AboutUsDialog

--- app/src/main/java/com/mls/mor/nytnews/ui/common/AboutUsDialog.kt
+++ app/src/main/java/com/mls/mor/nytnews/ui/common/AboutUsDialog.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -35,6 +37,7 @@ fun AboutUsDialog(
         ) {
             Column(
                 modifier = Modifier
+                    .verticalScroll(rememberScrollState())
                     .fillMaxWidth()
                     .padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
